69b5aef6133203be4ce1837101eedac576bc6933
Chris Meyer <cgmeyer@uchicago.edu>
2019-04-28 10:38:41 -0500

    updates

--- expansion/gen3cli.py
+++ expansion/gen3cli.py
@@ -74,6 +74,50 @@ class Gen3cli:
 		return df
 
 
+	def submit_file(self, project_id, filename, chunk_size=30, row_offset=0):
+		program,project = project_id.split('-',1)
+
+		df = Gen3cli.read_file(self, filename=filename)
+		total_rows = len(df)
+		print("Submitting "+filename+" with "+str(total_rows)+" records.")
+
+		start = row_offset
+		end = row_offset + chunk_size
+		chunk = df[start:end]
+		count = 0
+		responses = []
+		headers = {'content-type': 'text/tab-separated-values'}
+
+		while (start+len(chunk)) <= total_rows:
+			count+=1
+			print("Chunk "+str(count)+": ")
+			#itime = datetime.datetime.now()
+			api_url = "{}/api/v0/submission/{}/{}".format(self._endpoint, program, project)
+			#d = chunk.to_csv(sep='\t',index=False)
+			#requests.put(api_url, auth=auth, data=d,headers=headers).text
+			output = requests.put(api_url, auth=self._auth_provider, data=chunk.to_csv(sep='\t',index=False), headers=headers).text
+			print(output)
+			responses.append(output)
+			#print(api_url)
+			#print(chunk) #debug
+			responses.append(str(chunk)) #debug
+
+			if True:
+			#end of loop
+				start+=chunk_size
+				end = start + chunk_size
+				chunk = df[start:end]
+			else:
+				retry=1
+
+		return responses
+
+filename='subject_100.tsv'
+#	def submit_file(self, project_id, filename, chunk_size=30, row_offset=0):
+cli = Gen3cli(api, auth)
+res = cli.submit_file(filename=filename,project_id='DCF-demo')
+
+
 import gen3
 from gen3.auth import Gen3Auth
 from gen3.submission import Gen3Submission
@@ -93,135 +137,9 @@ d.head()
 d = cli.read_file('diagnosis_100.poop')
 d.head()
 
-	def submit_file(project,filename,api,length,row,outdir='./submission_output',format='tsv'):
-		"""Submit record(s) to a project as json.
+df = cli.read_file('subject_100.tsv')
 
-		Args:
-			program (str): The program to submit to.
-			project (str): The project to submit to.
-			json (object): The json defining the record(s) to submit. For multiple records, the json should be an array of records.
-
-		Examples:
-			This submits records to the CCLE project in the sandbox commons.
-
-			>>> Gen3Submission.submit_record("DCF", "CCLE", json)
-
-		"""
-		token_url   = api + "/user/credentials/cdis/access_token"
-		graphql_url = api + "/api/v0/submission/graphql/"
-		project_url = api + "/api/v0/submission/" + project.replace('-', '/', 1) + '/'
-
-		# # get keys
-		# json_data = open(args.authfile).read()
-		# keys = json.loads(json_data)
-		# auth = requests.post(token_url, json=keys)
-
-		header = ""
-		data = ""
-		count = 0
-		total = -1
-		nline = 0
-
-		# create output directory if doesn't exist
-		if not os.path.exists(outdir):
-			os.makedirs(outdir)
-
-		# if there are multiple dots, splitext splits at the last one (so splitext('file.jpg.zip') gives ('file.jpg', '.zip')
-		arg_filename = os.path.basename(filename)
-		outfile = outdir + "submission_output_" + os.path.splitext(filename)[0] + ".txt"
-		i = 2
-		while os.path.isfile(outfile):
-			outfile = outdir + "submission_output_" + os.path.splitext(arg_filename)[0] + "_" + str(i) + ".txt"
-			i += 1
-		output = open(outfile, 'w')
-
-		with open(filename, 'r') as file:
-			for line in file:
-				# if format is 'csv' or 'CSV':
-				# 	line = line.replace(",", "\t")
-				# if format is 'xlsx' or 'XLSX':
-				# 	#read the file in with pandas
-				if nline == 0:
-					header = line
-					data = header + "\r"
-				nline += 1
-				if nline > int(args.row):
-					data = data + line + "\r"
-					count = count + 1
-					total = total + 1
-					if count >= args.length:
-						count = 1
-						itime = datetime.datetime.now()
-
-						req = requests.Request(method='PUT', url=project_url, headers={'content-type': 'text/tab-separated-values', 'Authorization': 'bearer '+ auth.json()['access_token']}, data=data)
-						prepared = req.prepare()
-						response = requests.Session().send(prepared)
-						etime = datetime.datetime.now()
-						print ("Submitted (" + str(total) + "): " + str(response) + " " + str(etime-itime))
-						output.write("Submitted (" + str(total) + "): " + str(response))
-						output.write(response.text)
-						if "200" not in str(response):
-							print ("Submission failed. Stopping...")
-							break
-						data = header + "\r"
-
-		response = requests.put(project_url, data=data, headers={'content-type': 'text/tab-separated-values', 'Authorization': 'bearer '+ auth.json()['access_token']})
-		print ("Submitted (" + str(total) + "): " + str(response))
-		output.write("Submitted (" + str(total) + "): " + str(response))
-		output.write(response.text)
-
-
-
-
-
-
-
-	# from cgmeyer submission_v4
-		print("\n in upload mode \n") #debug
-		outfile = open_outfile()
-		headers['content-type']='text/tab-separated-values'
-		uurl = api + 'api/v0/submission/' + project.replace('-','/',1) + '/'
-		print('\nopening '+args.file+' \n\n')
-		with open(args.file, 'rU') as file:
-			lines = file.readlines()
-			print("\nFound " + str(len(lines)) + " lines in the tsv, including the header row.\n")
-		file.close #is this necessary?
-		header = data = ""
-		count = 0
-		total = -1
-		del lines
-		with open(args.file, 'rU') as file:
-			for line in file:
-				if is_csv:
-					line = line.replace(",", "\t")
-				if count == 0:
-					header = line
-				data = data + line + "\r"
-				count += 1
-				total += 1
-				if count >= args.length:
-					count = 1
-					response = requests.put(uurl, data=data, headers=headers)
-					if success.match(str(response)):
-						print("Succeeded (" + str(total) + "): " + str(response))
-					else:
-						print("Failed (" + str(total) + "): " + str(response))
-						failfile = open_failfile()
-						failfile.write(data)
-						failfile.close
-					outfile.write("Submitted (" + str(total) + "): " + str(response))
-					outfile.write(response.text)
-					data = header + "\r"
-		response = requests.put(uurl, data=data, headers=headers)
-		if fail.match(str(response)):
-			print("\nFailed\n")
-			failfile = open_failfile()
-			failfile.write(data)
-			failfile.close
-		else:
-			print("\nSuccess\n")
-		print("Submitted (" + str(total) + "): " + str(response))
-		outfile.write("Submitted (" + str(total) + "): " + str(response))
-		outfile.write(response.text)
-		outfile.close
-		print("\n finished upload script \n") #debug
+chunk1 = df[0:30]
+chunk2 = df[30:60]
+chunk3 = df[60:90]
+chunk4 = df[90:120]
