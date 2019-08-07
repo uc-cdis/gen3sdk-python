20d66d3e2c8b3350c9a1006f89fbd7b8483449a9
Chris Meyer <cgmeyer@uchicago.edu>
2019-08-07 15:58:01 -0500

    added fxn

--- expansion/expansion.py
+++ expansion/expansion.py
@@ -650,6 +650,22 @@ class Gen3Expansion:
             plt.title("PDF of "+numeric_property+' for ' + category +' (N = '+str(N)+')') # You can comment this line out if you don't need title
             plt.show(fig)
 
+    def plot_numeric_by_category(self, numeric_property, category_property, df):
+        sns.set(style="darkgrid")
+        N = len(df)
+        categories = list(set(df[category_property]))
+
+        for category in categories:
+            subset = df[df[category_property] == category]
+
+            fig = sns.distplot(subset[numeric_property].dropna(), hist = False, kde = True,
+                         bins = 3, kde_kws = {'linewidth': 2}, label = category)
+
+            plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        plt.figtext(.8, .8, 'N = '+str(N))
+        plt.title(numeric_property+' for ' + category +' (N = '+str(N)+')') # You can comment this line out if you don't need title
+        plt.show(fig)
+
     def plot_numeric_property_by_2_categories(self, numeric_property, category_property, category_property_2, df):
         #plot a histogram of numeric variable in a dataframe
         df = df[df[numeric_property].notnull()]
