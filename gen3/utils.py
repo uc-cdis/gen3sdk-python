817c82516acb923319f28c73faacc34d6d0595e2
Alexander VanTol <avantol13@gmail.com>
2020-03-16 13:43:00 -0500

    feat(utils): add common utils file for functions

---
+++ gen3/utils.py
@@ -0,0 +1,29 @@
+def append_query_params(original_url, **kwargs):
+    """
+    Add additional query string arguments to the given url.
+
+    Example call:
+        new_url = append_query_params(
+            original_url, error='this is an error',
+            another_arg='this is another argument')
+    """
+    scheme, netloc, path, query_string, fragment = urlsplit(original_url)
+    query_params = parse_qs(query_string)
+    if kwargs is not None:
+        for key, value in kwargs.items():
+            query_params[key] = [value]
+
+    new_query_string = urlencode(query_params, doseq=True)
+    new_url = urlunsplit((scheme, netloc, path, new_query_string, fragment))
+    return new_url
+
+
+def split_url_and_query_params(url):
+    """
+    Given a url, return the url (no query params) and the split out the
+    query paramaters separately
+    """
+    scheme, netloc, path, query_string, fragment = urlsplit(url)
+    query_params = parse_qs(query_string)
+    url = urlunsplit((scheme, netloc, path, None, fragment))
+    return url, query_params
