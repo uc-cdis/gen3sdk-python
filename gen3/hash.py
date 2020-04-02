be55a9ff35d6cd2a2c9fb6d9712072314b37368a
Giang Bui <giangbui0816@gmail.com>
2020-04-02 13:02:14 -0500

    feat(hash): introduce Gen3Hash class

---
+++ gen3/hash.py
@@ -0,0 +1,71 @@
+import os
+import hashlib
+import asyncio
+import logging
+from concurrent.futures import ProcessPoolExecutor
+
+
+CHUNK_SIZE = 4 * 1024
+
+
+class Gen3Hash():
+    def __init__(self, file_path, chunk_size=CHUNK_SIZE):
+        self.file_path = file_path
+        self.chunk_size = chunk_size
+
+    def get_hash(self, hash_type):
+        """
+        Compute hash
+
+        Args:
+            hash_type(str): the type of hash needs to be computed
+
+        Returns:
+            str: hash string
+        """
+
+        try:
+            hash = getattr(hashlib, hash_type)()
+        except AttributeError as e:
+            logging.error("hashlib does not have {} as its function".format(hash_type))
+            return None
+        with open(self.file_path, "rb") as f:
+            while True:
+                chunk = f.read(CHUNK_SIZE)
+                if not chunk:
+                    break
+                hash.update(chunk)
+            return hash.hexdigest()
+
+    async def get_hash_async(self, hash_types, queue):
+        """
+        This function computes multiple hash types 
+
+        Args:
+            hash_types(list(str)): list of hash types
+            queue(asyncio.Queue()): output queue
+
+        """
+        ec = ProcessPoolExecutor()
+       
+        for f in asyncio.as_completed(
+            [
+                loop.run_in_executor(ec, gen3Hash.get_hash, hash_type)
+                for hash_type in hash_types
+            ],
+        ):
+            hash = await f
+            await queue.put(hash)
+
+
+if __name__ == "__main__" :
+    gen3Hash = Gen3Hash("./mymovie1.mov")
+    gen3Hash.get_hash("md5")
+
+    start = time.time()
+    hashes = asyncio.Queue()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(gen3Hash.get_hash_async(["md5", "sha12"], hashes))
+    end = time.time()
+    print(hashes)
+    logging.info("Async:", end - start)
