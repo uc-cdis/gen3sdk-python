78d65ec122befd04fb4c07407c5f4ba8f4695a61
Chris Meyer <cgmeyer@uchicago.edu>
2020-04-23 11:55:10 -0500

    updates

--- expansion/summarize_data_commons.py
+++ expansion/summarize_data_commons.py
@@ -67,6 +67,8 @@ exp = Gen3Expansion(api, auth) # Initialize an instance, using its functions lik
 #     from io import StringIO
 
 
+import statistics
+
 def t(var):
     vtype = type(var)
     print(vtype)
@@ -76,19 +78,17 @@ def t(var):
         print("{}".format(var))
 
 
-def get_output_name(name,extension,commons,outdir='reports'):
-    dcs = list(commons.keys())
-    outname = "{}_".format(name)
-    for i in range(len(dcs)):
-        outname += dcs[i]
-        if i != len(dcs)-1:
-            outname += '_'
+def get_output_name(name,extension='tsv',outdir='data_summary_reports'):
+    names = name.split('/')
+    names = [name for name in names if name != '']
+    basename = names[-1]
+    outname = "data_summary_{}".format(basename)
     outname += '.{}'.format(extension)
-    outname = "{}/{}".format(outdir,outname)#reports/summary_staging_prod.tsv
+    outname = "{}/{}".format(outdir,outname)# data_summary_reports/summary_staging_prod.tsv
     return outname
 
 
-def create_output_dir(outdir='reports'):
+def create_output_dir(outdir='data_summary_reports'):
     cmd = ['mkdir','-p',outdir]
     try:
         output = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode('UTF-8')
@@ -160,12 +160,12 @@ def summarize_dd(api,props_to_remove=['case_submitter_id'],nodes_to_remove=['roo
 ########################################################################################################################
 ########################################################################################################################
 
-def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project_id','type','id','submitter_id','case_submitter_id','md5sum','file_name','object_id'],omit_nodes=['metaschema','root','program','project','data_release'],outdir='data_summary_reports'):
+def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project_id','type','id','submitter_id','case_submitter_id','case_ids','visit_id','sample_id','md5sum','file_name','object_id'],omit_nodes=['metaschema','root','program','project','data_release'],outdir='data_summary_reports'):
     """
-    Returns a nested dictionary of summarized TSV data per commons, project, node, and property.
-    For each property, the total number of records in the project with and without data is returned.
-    Bins and the number of unique bins are returned for string, enumeration and boolean properties.
-    The mean, median, min, max, and stdev are returned for integers and numbers.
+    Returns a nested dictionary of summarized TSV data per project, node, and property.
+    For each property in each project, the total, non-null and null counts are returned.
+    For string, enumeration and boolean properties, bins and the number of unique bins are returned.
+    For integers and numbers, the mean, median, min, max, and stdev are returned.
     Outliers in numeric data are identified using "+/- stdev". The cut-off for outlier identification can be changed by raising or lowering the outlier_threshold (default=3).
 
     Args:
@@ -174,14 +174,16 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
         prefix(str): Default gets TSVs from all directories ending in "_tsvs". "prefix" of the project_tsvs directories (e.g., program name of the projects: "Program_1-Project_2_tsvs"). Result of running the Gen3Expansion.get_project_tsvs() function.
         outlier_threshold(number): The upper/lower threshold for identifying outliers in numeric data is the standard deviation multiplied by this number.
         omit_props(list): Properties to omit from being summarized. It doesn't make sense to summarize certain properties, e.g., those with all unique values. May want to omit: ['sample_id','specimen_number','current_medical_condition_name','medical_condition_name','imaging_results','medication_name'].
+        omit_nodes(list): Nodes in the data dictionary to omit from being summarized, e.g., program, project, data_release, root and metaschema.
         outdir(str): A directory for the output files.
 
     Examples:
-        s = summarize_tsvs(tsv_dir,
-            dd,
+        s = summarize_tsvs(tsv_dir='/Users/christopher/Documents/Notes/ACCT/tsvs/project_tsvs_04222020/',
+            dd=dd,
             prefix='',
             outdir='data_summary_reports',
             omit_props=['project_id','type','id','submitter_id','case_ids','md5sum','file_name','object_id'],
+            omit_nodes=['metaschema','root','program','project','data_release'],
             outlier_threshold=3)
     """
     summary = {}
@@ -191,7 +193,7 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
     dir_pattern = "{}*{}".format(prefix,'tsvs')
     project_dirs = glob.glob(dir_pattern)
 
-    data,nn_nodes,nn_props,null_nodes,null_props = {},[],[],[],[]
+    data,nn_nodes,nn_props,null_nodes,null_props,all_prop_ids = {},[],[],[],[],[]
 
     for project_dir in project_dirs: # project_dir=project_dirs[0]
 
@@ -200,7 +202,7 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
         except:
             print("Couldn't extract the project_id from {}!".format(project_dir))
 
-        os.chdir("{}{}".format(tsv_dir,project_dir))
+        os.chdir("{}/{}".format(tsv_dir,project_dir))
         print("\tSummarizing data in project '{}'".format(project_id))
 
         fpattern = "{}*{}".format(prefix,'.tsv')
@@ -208,7 +210,7 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
         print("\t\tFound the following {} TSVs: {}".format(len(fnames),fnames))
 
         data[project_id] = {}
-        data[project_id]['nodes'] = {}
+        data[project_id]['nodes'] = {} # currently 'nodes' is the only key in a project's dictionary, but leaving it for now in case I want to add other project specific stats
 
         for fname in fnames: # Each node with data in the project is in one TSV file so len(fnames) is the number of nodes in the project with data.
 
@@ -237,9 +239,11 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
 
                     data[project_id]['nodes'][node][prop] = {}
 
+                    # because of sheepdog bug, need to inclue "None" in "null" (:facepalm:) https://ctds-planx.atlassian.net/browse/PXP-5663
+                    df.at[df[prop] == 'None',prop] = np.nan
                     null = df.loc[df[prop].isnull()]
                     nn = df.loc[df[prop].notnull()]
-                    nn = df.loc[df[prop]!='None'] # remove this after sheepdog bug fix is deployed:  https://ctds-planx.atlassian.net/browse/PXP-5663
+
                     ptype = get_prop_type(node,prop,dd)
 
                     data[project_id]['nodes'][node][prop]["N"] = len(df)
@@ -248,16 +252,18 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
                     data[project_id]['nodes'][node][prop]["type"] = ptype
 
                     prop_name = "{}.{}".format(node,prop)
+                    prop_id = "{}.{}".format(project_id,prop_name)
 
                     if nn.empty:
-                        print("\t\tAll null data for '{}' in this TSV.".format(prop))
+                        #print("\t\tAll null data for '{}' in this TSV.".format(prop))
                         null_props.append(prop_name)
 
                     else:
                         nn_props.append(prop_name)
+                        all_prop_ids.append(prop_id)
 
                         # Get stats for strings
-                        if ptype in ['string','enum','boolean','date']:
+                        if ptype in ['string','enum','boolean','date','array']:
 
                             counts = Counter(nn[prop])
                             df1 = pd.DataFrame.from_dict(counts, orient='index').reset_index()
@@ -292,7 +298,7 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
 
                             # if property type is 'integer', change min, max, median to int type
                             if ptype == 'integer':
-                                median = int(md) # median
+                                median = int(median) # median
                                 minimum = int(minimum) # min
                                 maximum = int(maximum) # max
                                 outliers = [int(i) for i in outliers] # convert outliers from float to int
@@ -307,11 +313,11 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
                             print("\t\t'{}.{}.{}': {}".format(project_id,node,prop,data[project_id]['nodes'][node][prop]))
 
                         else: # If its not in the list of ptypes, exit. Need to add array handling.
-                            print("\t\tUnhandled property type {}".format(prop))
-                            exit(1)
-
+                            print("\t\tUnhandled property type {}: {}".format(prop,ptype))
+                            exit()
 
     summary['data'] = data
+    summary['all_prop_ids'] = all_prop_ids
 
     # summarize all properties
     nn_props = sorted(list(set(nn_props)))
@@ -335,82 +341,112 @@ def summarize_tsvs(tsv_dir,dd,prefix='',outlier_threshold=3,omit_props=['project
     return summary
 
 
-s = summarize_tsvs(tsv_dir,dd)
+########################################################################################################################
+
+# Usage example:
+
+# tsv_dir = '/Users/christopher/Documents/Notes/ACCT/tsvs/project_tsvs_04222020/'
+# dd = sub.get_dictionary_all()
+#
+# tsv_dir = '/Users/christopher/Documents/Notes/BHC/data_qc/dm2.2_QC/prep_tsvs_04152020'
+# prep_dd = prep_sub.get_dictionary_all()
+#s = summarize_tsvs(tsv_dir,dd)
+
 
 ########################################################################################################################
 ########################################################################################################################
+########################################################################################################################
 
-def write_commons_report(summary,bin_limit=False,create_report=True,report_null=True,outdir='data_summary_reports',tsv_dir="."):
+def write_commons_report(summary,tsv_dir,outdir='.',bin_limit=False,create_report=True,report_null=True):
     """ Converts the summarize_tsvs() dictionary into a pandas DataFrame and writes it to a file.
     Args:
         summary(dict): the dict returned from running 'summarize_tsvs()' script.
+        tsv_dir(str): the directory where project TSV folders and 'outdir' are.
+        outdir(str): Directory to write the report file to.
         bin_limit(int): limits the number of bins written to the report for enums, strings, and booleans. If bin_limit=3, only the largest 3 bins will be reported.
         create_report(bool): whether to write a TSV report to the outdir
         report_null(bool): if False, properties in TSVs with entirely null data will be excluded from the report.
-        outdir(str): Directory to write the report file to.
-        tsv_dir(str): the directory where project TSV folders and 'outdir' are.
+
+    Example:
+        r = write_commons_report(summary = s,
+            tsv_dir = '/Users/christopher/Documents/Notes/BHC/data_migration/v2.2/prod_tsvs_04112020',
+            outdir = '.'
+            bin_limit = False,
+            create_report = True,
+            report_null = True)
+
     """
 
-
-    total_props = 0
-    for dc in dcs:
-        total_props += summary[dc]['data_count']
+    # count the total number of properties in "summary" to write to report
+    total_props = len(summary['all_prop_ids'])
 
     report = pd.DataFrame(index=range(0,total_props),
-        columns=['project','node','property','property_type',
+        columns=['prop_id','project_id','node','property','property_type',
                 'N','nn','null','all_null',
                 'min','max','median','mean','stdev','outliers',
                 'bin_number','bins'])
 
     i = 0
 
-        projects = list(summary[dc]['data'].keys())
-        for project in projects: #project=projects[0]
-            nodes = list(summary[dc]['data'][project])
-            for node in nodes: #node=nodes[0]
-                props = list(summary[dc]['data'][project][node]['properties'])
-                for prop in props:
-                    print("Writing '{}' '{}' '{}' '{}' to report.".format(dc,project,node,prop))
-                    data = summary[dc]['data'][project][node]['properties'][prop]
-                    total_records = data['total_records']
-                    null_count = data['null_count']
+    project_ids = list(s['data'])
 
-                    if total_records > null_count or report_null is True:
-                        report['total_records'][i] = total_records
-                        report['null_count'][i] = null_count
-                        report['commons'][i] = dc
-                        report['project'][i] = project
-                        report['node'][i] = node
-                        report['property'][i] = prop
-                        report['property_type'][i] = data['property_type']
-                        if 'stats' in data:
-                            report['N'][i] = data['stats']['N']
-                            if data['property_type'] in ['string','enum','boolean']:
-                                prop_bins = data['stats']['bins']
-                                bin_number = len(prop_bins)
-                                report['bin_number'][i] = bin_number
-                                if not bin_limit is False and bin_number > bin_limit:
-                                    report['bins'][i] = prop_bins[:bin_limit]
-                                else:
-                                    report['bins'][i] = prop_bins
-                            elif data['property_type'] in ['integer','number']:
-                                report['min'][i] = data['stats']['min']
-                                report['max'][i] = data['stats']['max']
-                                report['mean'][i] = data['stats']['mean']
-                                report['median'][i] = data['stats']['median']
-                                report['stdev'][i] = data['stats']['stdev']
-                                report['outliers'][i] = data['stats']['outliers']
-                        if total_records == null_count:
-                            report['all_null'][i] = True
+    for project_id in project_ids: #project_id=project_ids[0]
+        print("Reporting '{}' data:".format(project_id))
+        nodes = list(summary['data'][project_id]['nodes'])
+        for node in nodes: #node=nodes[0]
+            props = list(summary['data'][project_id]['nodes'][node])
+            for prop in props: #prop=props[0]
+
+                prop_id = "{}.{}.{}".format(project_id,node,prop)
+                stats = summary['data'][project_id]['nodes'][node][prop]
+
+                if stats['null'] == stats['N'] and report_null is False:
+                    print("\t'{}' not written to report. All records are null and 'report_null' option is set to 'False'!".format(prop_id))
+
+                else:
+
+                    report['prop_id'][i] = prop_id
+                    report['project_id'][i] = project_id
+                    report['node'][i] = node
+                    report['property'][i] = prop
+
+                    report['N'][i] = stats['N']
+                    report['nn'][i] = stats['nn']
+                    report['null'][i] = stats['null']
+                    report['property_type'][i] = stats['type']
+
+                    if stats['nn'] == 0:
+                        report['all_null'][i] = True
+                    else:
+                        report['all_null'][i] = False
+
+                        if stats['type'] in ['string','enum','boolean','array']:
+                            report['bin_number'][i] = stats['bin_number']
+
+                            bins = stats['bins']
+                            if not bin_limit is False and stats['bin_number'] > bin_limit:
+                                report['bins'][i] = stats['bins'][:bin_limit]
+                            else:
+                                report['bins'][i] = stats['bins']
+
+                        elif stats['type'] in ['integer','number']:
+                            report['min'][i] = stats['min']
+                            report['max'][i] = stats['max']
+                            report['mean'][i] = stats['mean']
+                            report['median'][i] = stats['median']
+                            report['stdev'][i] = stats['stdev']
+                            report['outliers'][i] = stats['outliers']
+
                         else:
-                            report['all_null'][i] = False
-                        i += 1
-    report['prop_id'] = report['project'] + '.' + report['node'] + '.' + report['property']
+                            print("Unhandled property: '{}' of type '{}'!".format(prop_id,stats['type']))
+
+                    print("\t'{}' written to report ({} total, {} null, {} non-null).".format(prop_id,stats['N'],stats['null'],stats['nn']))
+                    i += 1
 
     if create_report is True:
-        os.chdir(home_dir)
-        create_output_dir()
-        outname = get_output_name('report','tsv',summary,outdir)
+        os.chdir(tsv_dir)
+        create_output_dir(outdir='data_summary_reports')
+        outname = get_output_name(name=tsv_dir,extension='tsv',outdir='data_summary_reports')
         report.to_csv(outname, sep='\t', index=False, encoding='utf-8')
         print("\nReport written to file:\n\t{}".format(outname))
 
@@ -418,7 +454,9 @@ def write_commons_report(summary,bin_limit=False,create_report=True,report_null=
 
 
 ########################################################################################################################
-# report = write_commons_report(summary,bin_limit=False,create_report=True,outdir='reports',home_dir='/Users/christopher/Documents/Notes/BHC/data_qc/')
+
+tsv_dir = '/Users/christopher/Documents/Notes/ACCT/tsvs/project_tsvs_04222020/'
+report = write_commons_report(summary=s, outdir='.', tsv_dir=tsv_dir)
 
 
 ########################################################################################################################
