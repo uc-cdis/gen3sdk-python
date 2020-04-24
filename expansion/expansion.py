2f71ef5a464b0e98ec9d2fbe00e3b9c4a29316b6
Chris Meyer <cgmeyer@uchicago.edu>
2020-04-24 11:37:18 -0500

    updates

--- expansion/expansion.py
+++ expansion/expansion.py
@@ -2347,12 +2347,14 @@ class Gen3Expansion:
                                 # make a list of the data values as floats (converted from strings)
                                 nn_all = nn[prop]
                                 nn_num = pd.to_numeric(nn[prop], errors='coerce').dropna()
-                                d_all = list(nn0)
-                                d_num = list(nn1)
+                                d_all = list(nn_all)
+                                d_num = list(nn_num)
                                 if len(d_all) > len(d_num):
                                     non_numbers = [element for element in d_all if element not in list(map(str,d_num))]
                                     print("\t\tFound {} string values among the {} records of prop '{}' with value(s): {}. Calculating stats only for the {} numeric values.".format(len(non_numbers),len(nn),prop,list(set(non_numbers)),len(d_num)))
+
                                 # calculate summary stats using the float list d
+                                d = d_num
                                 mean = statistics.mean(d)
                                 median = statistics.median(d)
                                 minimum = min(d)
