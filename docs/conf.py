f6d754a023501a80270d0f5500c9eda4785200f8
Pauline <ribeyre@uchicago.edu>
2020-12-11 15:51:41 -0600

    ?

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
+print('os.environ["POETRY_VIRTUAL_ENV"]', os.environ["POETRY_VIRTUAL_ENV"])
 sys.path.insert(0, os.environ["POETRY_VIRTUAL_ENV"])
 
 
