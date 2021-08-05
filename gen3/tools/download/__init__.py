f92f148cc9ae8231a73968cacba65ad8eab09b5f
Zakir Gowani <zakirgowani@gmail.com>
2021-08-05 11:43:21 -0700

    rename functin

--- gen3/tools/download/__init__.py
+++ gen3/tools/download/__init__.py
@@ -1,3 +1,3 @@
 from gen3.tools.download.manifest import describe_access_to_files_in_workspace_manifest
 from gen3.tools.download.manifest import list_files_in_workspace_manifest
-from gen3.tools.download.manifest import download_workspace_manifest
+from gen3.tools.download.manifest import download_files_in_workspace_manifest
