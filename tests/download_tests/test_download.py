5dc2ecd4fa3b4b51b54ce20b313d0a2faa78e59d
Craig Barnes <craigrbarnes@uchicago.edu>
2021-09-08 14:40:06 -0500

    Refactored DRS support for downloading manifests

--- tests/download_tests/test_download.py
+++ tests/download_tests/test_download.py
@@ -17,57 +17,60 @@ from gen3.tools.download.drs_download import (
     Manifest,
     DRSObjectType,
     DownloadManager,
-
     wts_external_oidc,
-    resolve_compact_drs_using_dataguids
+    resolve_compact_drs_using_dataguids,
 )
 
 
 @pytest.fixture
 def drs_objects():
-    with open(Path(DIR, 'resources/drs_objects.json')) as fin:
+    with open(Path(DIR, "resources/drs_objects.json")) as fin:
         test_data = json.load(fin)
-        return test_data['drs_objects']
+        return test_data["drs_objects"]
 
 
 @pytest.fixture
 def drs_resolver_dataguids():
-    with open(Path(DIR, 'resources/dataguids_commons1.json')) as fin:
+    with open(Path(DIR, "resources/dataguids_commons1.json")) as fin:
         return json.load(fin)
 
 
 @pytest.fixture
 def wts_oidc():
-    with open(Path(DIR, 'resources/wts_oidc.json')) as fin:
+    with open(Path(DIR, "resources/wts_oidc.json")) as fin:
         return json.load(fin)
 
 
 @pytest.mark.parametrize(
-    "identifier,expected", [
-        ("drs://jcoin.datacommons.io/dg.XXTS/008e6b62-28c0-4659-afec-622340b0ef76",
-         ('jcoin.datacommons.io', 'hostname')),
-        ("dg.XXTS/008e6b62-28c0-4659-afec-622340b0ef76", ('dg.XXTS', 'compact')),
-        ("008e6b62-28c0-4659-afec-622340b0ef76", ('', 'unknown'))
-    ]
+    "identifier,expected",
+    [
+        (
+            "drs://jcoin.datacommons.io/dg.XXTS/008e6b62-28c0-4659-afec-622340b0ef76",
+            ("jcoin.datacommons.io", "hostname"),
+        ),
+        ("dg.XXTS/008e6b62-28c0-4659-afec-622340b0ef76", ("dg.XXTS", "compact")),
+        ("008e6b62-28c0-4659-afec-622340b0ef76", ("", "unknown")),
+    ],
 )
 def test_parse_drs_identifier(identifier, expected):
     assert parse_drs_identifier(identifier) == expected
 
 
 @pytest.mark.parametrize(
-    "s,expected", [
-        ("https://jcoin.datacommons.io/index/", 'jcoin.datacommons.io'),
-        ("http://foo.testgen3.io/index/", 'foo.testgen3.io'),
-        ("jcoin.datacommons.io/index/", 'jcoin.datacommons.io'),
-        ("jcoin.datacommons.io/index", 'jcoin.datacommons.io'),
-    ]
+    "s,expected",
+    [
+        ("https://jcoin.datacommons.io/index/", "jcoin.datacommons.io"),
+        ("http://foo.testgen3.io/index/", "foo.testgen3.io"),
+        ("jcoin.datacommons.io/index/", "jcoin.datacommons.io"),
+        ("jcoin.datacommons.io/index", "jcoin.datacommons.io"),
+    ],
 )
 def test_strip_http_url(s: str, expected):
     assert strip_http_url(s) == expected
 
 
 def test_load_manifest():
-    object_list = Manifest.load(Path(DIR, 'resources/manifest_test_drs_compact.json'))
+    object_list = Manifest.load(Path(DIR, "resources/manifest_test_drs_compact.json"))
     object_list = [asdict(x) for x in object_list]
 
     with open("expected/manifest_test_drs_compact_object_list.json", "rt") as fin:
@@ -77,53 +80,55 @@ def test_load_manifest():
 
 
 @pytest.mark.parametrize(
-    "index,expected", [
-        (0, 'TestDataSet1.sav'),
-        (1, 'TestDataSet_April2020.sav'),
-    ]
+    "index,expected",
+    [
+        (0, "TestDataSet1.sav"),
+        (1, "TestDataSet_April2020.sav"),
+    ],
 )
 def test_extract_filename_from_drs_object(drs_objects, index, expected):
     assert extract_filename_from_object_info(drs_objects[index]) == expected
 
 
 @pytest.mark.parametrize(
-    "index,expected", [
+    "index,expected",
+    [
         (0, DRSObjectType.object),
-    ]
+    ],
 )
 def test_get_drs_object_type(drs_objects, index, expected):
     assert get_drs_object_type(drs_objects[index]) == expected
 
 
-@pytest.mark.parametrize(
-    "hostname", [
-        ("test.datacommons.io")
-    ]
-)
+@pytest.mark.parametrize("hostname", [("test.datacommons.io")])
 def test_get_external_wts_oidc(wts_oidc, hostname):
     with requests_mock.Mocker() as m:
         m.get(f"https://{hostname}/wts/external_oidc/", json=wts_oidc[hostname])
         expected = {
-            'test.commons1.io': {
-                'base_url': 'https://test.commons1.io',
-                'idp': 'test-google',
-                'name': 'TEST1 Google Login',
-                'refresh_token_expiration': '9 days',
-                'urls': [{
-                    'name': 'TEST Google Login',
-                    'url': 'https://test.datacommons.io/wts/oauth2/authorization_url?idp=test-google'
-                }]
+            "test.commons1.io": {
+                "base_url": "https://test.commons1.io",
+                "idp": "test-google",
+                "name": "TEST1 Google Login",
+                "refresh_token_expiration": "9 days",
+                "urls": [
+                    {
+                        "name": "TEST Google Login",
+                        "url": "https://test.datacommons.io/wts/oauth2/authorization_url?idp=test-google",
+                    }
+                ],
             },
-            'externaldata.commons2.org': {
-                'base_url': 'https://externaldata.commons2.org',
-                'idp': 'externaldata-google',
-                'name': 'FAIR Repository Google Login',
-                'refresh_token_expiration': '7 days',
-                'urls': [{
-                    'name': 'FAIR Repository Google Login',
-                    'url': 'https://test.datacommons.io/wts/oauth2/authorization_url?idp=externaldata-google'
-                }]
-            }
+            "externaldata.commons2.org": {
+                "base_url": "https://externaldata.commons2.org",
+                "idp": "externaldata-google",
+                "name": "FAIR Repository Google Login",
+                "refresh_token_expiration": "7 days",
+                "urls": [
+                    {
+                        "name": "FAIR Repository Google Login",
+                        "url": "https://test.datacommons.io/wts/oauth2/authorization_url?idp=externaldata-google",
+                    }
+                ],
+            },
         }
         results = wts_external_oidc(hostname)
         assert results == expected
@@ -137,9 +142,7 @@ def test_get_external_wts_oidc(wts_oidc, hostname):
 
 def test_resolve_compact_drs_using_dataguids(drs_objects):
     DRS_RESOLVER_RESULTS = {
-        "acl": [
-            "admin"
-        ],
+        "acl": ["admin"],
         "authz": [],
         "baseid": "1e6cf3f1-a5af-4543-a1ca-84b41b3221a9",
         "created_date": "2018-06-13T17:16:29.981618",
@@ -148,11 +151,9 @@ def test_resolve_compact_drs_using_dataguids(drs_objects):
         "form": "object",
         "from_index_service": {
             "host": "https://test.commons1.io/index/",
-            "name": "TESTCommons"
+            "name": "TESTCommons",
         },
-        "hashes": {
-            "md5": "4fe23cf4c6bdcf930df5765b48d81216"
-        },
+        "hashes": {"md5": "4fe23cf4c6bdcf930df5765b48d81216"},
         "metadata": {},
         "rev": "6046fb9f",
         "size": 9567026,
@@ -160,13 +161,13 @@ def test_resolve_compact_drs_using_dataguids(drs_objects):
         "uploader": None,
         "urls": [
             "gs://test_workflow_testing/topmed_aligner/input_files/176325.0005.recab.cram",
-            "s3://test-workflow-testing/topmed-aligner/input-files/176325.0005.recab.cram"
+            "s3://test-workflow-testing/topmed-aligner/input-files/176325.0005.recab.cram",
         ],
         "urls_metadata": {
             "gs://test_workflow_testing/topmed_aligner/input_files/176325.0005.recab.cram": {},
-            "s3://test-workflow-testing/topmed-aligner/input-files/176325.0005.recab.cram": {}
+            "s3://test-workflow-testing/topmed-aligner/input-files/176325.0005.recab.cram": {},
         },
-        "version": None
+        "version": None,
     }
 
     object_id = "dg.XXTS/b96018c5-db06-4af8-a195-28e339ba815e"
@@ -175,7 +176,7 @@ def test_resolve_compact_drs_using_dataguids(drs_objects):
     DRS_MDS_RESULTS = {
         "host": "https://test.commons1.io/index/",
         "name": "TestCommons",
-        "type": "indexd"
+        "type": "indexd",
     }
 
     with requests_mock.Mocker() as m:
@@ -192,25 +193,31 @@ def test_resolve_compact_drs_using_dataguids(drs_objects):
         expected = "test.commons1.io"
         assert results == expected
 
-        m.get(f"https://dataguids.org/mds/metadata/{identifier}", json=DRS_MDS_RESULTS, status_code=404)
+        m.get(
+            f"https://dataguids.org/mds/metadata/{identifier}",
+            json=DRS_MDS_RESULTS,
+            status_code=404,
+        )
         assert resolve_compact_drs_using_dataguids(identifier, object_id) is None
 
+
 @pytest.mark.parametrize(
-    "hostname", [
-        ('test.datacommons.io'),
-    ]
+    "hostname",
+    [
+        ("test.datacommons.io"),
+    ],
 )
 def test_resolve_objects(wts_oidc, hostname):
 
     # TODO: WIP - need to complete Gen3Auth mocks
     test_key = {
         "api_key": "whatever."
-                   + base64.urlsafe_b64encode(
+        + base64.urlsafe_b64encode(
             ('{"iss": "%s", "exp": %d }' % (hostname, time.time() + 300)).encode(
                 "utf-8"
             )
         ).decode("utf-8")
-                   + ".whatever"
+        + ".whatever"
     }
 
     # with requests_mock.Mocker() as m:
