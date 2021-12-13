340224c4b7bd3ca385dd248b6dfae87e1cf497c0
Craig Barnes <craigrbarnes@uchicago.edu>
2021-12-13 11:48:08 -0600

    test update of poetry to prevent No module named virtualenv.activation.nushell

--- tests/jenkinsSmoke.sh
+++ tests/jenkinsSmoke.sh
@@ -14,6 +14,7 @@ cd ..
 export PATH="/var/jenkins_home/.local/bin:$PATH"
 export GEN3_API_KEY="accesstoken:///$(gen3 api access-token cdis.autotest@gmail.com)"
 
+poetry self update
 poetry config virtualenvs.path "${WORKSPACE}/pysdkvirtenv" --local
 poetry env use python3
 poetry install -vv
