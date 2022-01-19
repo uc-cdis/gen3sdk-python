5210c4063513fa303eb79a411b6eb18ec313f230
MaribelleHGomez <maribellehgomez@gmail.com>
2022-01-19 14:34:06 -0600

    added diff function

---
+++ gen3/tools/diff.py
@@ -0,0 +1,181 @@
+"""
+Takes the differential between the matching columns of two manifests
+
+TODO: Able to handle situations where only specific columns should be compared
+"""
+
+import os
+import logging
+import csv
+import copy
+
+from collections import OrderedDict
+
+
+def manifest_diff(
+    directory=".",
+    files=None,
+    allow_additional_columns=False,
+    output_manifest_file_delimiter=None,
+    output_manifest="diff-manifest.tsv",
+    **kwargs,
+):
+    """
+    Args:
+        directory(str):
+            Path of the directory containing the input manifests. All of the manifests contained in directory are assumed to be in a delimiter-separated values (DSV) format, and that there are no other non-DSV files in directory.
+        files(list[str]):
+            List of paths containing the input manifests. All of the manifests contained in directory are assumed to be in a delimiter-separated values (DSV) format, and that there are no other non-DSV files in directory
+        manifest_content(dict(dict)):
+            Dict of manifest content being compared
+        allow_additional_columns(boolean):
+            Bool allowing manifests to have different columns
+        output_manifest_file_delimiter(str):
+            The delimiter used for writing the output manifest. If not provided, the delimiter will be determined
+            based on the file extension of output_manifest
+        output_manifest(str):
+            The file to write the output manifest to
+
+    Returns:
+        None
+    """
+
+    files = files or []
+    if not files:
+        logging.info(f"Iterating over manifests in {directory} directory")
+        for file in sorted(os.listdir(directory)):
+            files.append(os.path.join(directory, file))
+
+    content = _precheck_manifests(
+        allow_additional_columns=allow_additional_columns,
+        files=files,
+    )
+
+    if content:
+        diff_content = _compare_manifest_columns(
+            allow_additional_columns=allow_additional_columns,
+            manifest_content=content,
+        )
+
+        _write_csv(
+            output_manifest_file_delimiter=output_manifest_file_delimiter,
+            output_manifest=output_manifest,
+            diff_content=diff_content,
+        )
+
+
+def _precheck_manifests(
+    allow_additional_columns,
+    files=[],
+    **kwargs,
+):
+    """
+    Args:
+        allow_additional_columns(bool)
+        files(list[str])
+
+    Returns:
+        List of CSVDict and headers if pass all checks
+    """
+
+    logging.info(f"Prechecking manifest files: {files}")
+
+    if not len(files) > 1:
+        logging.error("Must take difference of more than file")
+        return False
+
+    tsv_files = [file_name for file_name in files if ".tsv" in file_name.lower()]
+    if len(tsv_files) == len(files):
+        file_delimiter = "\t"
+    elif len(tsv_files) == 0:
+        file_delimiter = ","
+    else:
+        logging.error("Not all files have the same extension type")
+        return False
+
+    headers = set()
+    manifest_content = []
+    for manifest in files:
+        with open(manifest, "r", encoding="utf-8-sig") as csvfile:
+            csv_reader = csv.DictReader(csvfile, delimiter=file_delimiter)
+
+            field_names = csv_reader.fieldnames
+            logging.debug(f"Field names from {manifest}: {field_names}")
+            headers.update(set(field_names))
+            if not allow_additional_columns:
+                if not len(field_names) == len(headers):
+                    logging.error("Headers are not the same among manifests")
+                    return False
+
+            content = []
+            for row in csv_reader:
+                content.append(row)
+
+            manifest_content.append({"csvdict": content, "headers": headers})
+
+    return manifest_content
+
+
+def _compare_manifest_columns(
+    allow_additional_columns,
+    manifest_content=[],
+    **kwargs,
+):
+    """
+    Args:
+        manifest_content(list(dict)): List of CSVDict and headers
+        allow_additional_columns(bool)
+
+    Returns:
+        Dict containing list of diff and headers
+    """
+
+    headers = manifest_content[0]["headers"]
+    diff_content = []
+    # TODO make it so that not limited to only two list being compared
+    # also make comparing logic more efficient
+    # also make it so only certain headers being compared
+    for i in manifest_content[0]["csvdict"]:
+        if i not in manifest_content[1]["csvdict"]:
+            diff_content.append(i)
+
+    return {"headers": headers, "csvdict": diff_content}
+
+
+def _write_csv(output_manifest_file_delimiter, output_manifest, diff_content={}):
+    """
+    write to csv file
+
+    Args:
+        output_manifest_file_delimiter(str): file name
+        output_manifest: file name with extension
+        diff_content(dict(list)): dict of file info
+
+    Returns:
+        None.
+        Writes a manifest file of the diff between manifests
+    """
+
+    if output_manifest_file_delimiter is None:
+        output_manifest_file_ext = os.path.splitext(output_manifest)
+        if output_manifest_file_ext[-1].lower() == ".tsv":
+            output_manifest_file_delimiter = "\t"
+        else:
+            output_manifest_file_delimiter = ","
+
+    # figure headers
+    headers = diff_content["headers"]
+
+    logging.info(f"Writing diff manifest to {output_manifest}")
+    with open(output_manifest, "w") as outputfile:
+        output_writer = csv.DictWriter(
+            outputfile,
+            delimiter=output_manifest_file_delimiter,
+            fieldnames=headers,
+        )
+        output_writer.writeheader()
+
+        for record in diff_content["csvdict"]:
+            output_writer.writerow(record)
+
+        logging.info(f"Finished writing merged manifest to {output_manifest}")
