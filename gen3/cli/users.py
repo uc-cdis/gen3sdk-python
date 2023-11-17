cecf2562e65d5745db71cce6e49ff7615f7575aa
George Thomas <george42@uchicago.edu>
2023-11-17 14:02:02 -0800

    (fix PXP-9174): add gen3users cli

---
+++ gen3/cli/users.py
@@ -0,0 +1,39 @@
+import click
+
+# Temp for local import
+# import sys
+# sys.path.append('../')
+# import gen3users.gen3users.main as users_cli
+
+# Use this after gen3users merge
+from gen3users import main as users_cli
+
+# import gen3users as users_cli
+
+
+# Do we need these?
+try:
+    from importlib.metadata import entry_points
+except ImportError:
+    from importlib_metadata import entry_points
+
+
+@click.group()
+def main():
+    """Gen3 Command Line Interface"""
+    pass
+
+
+@click.group()
+def users():
+    """Commands for working with gen3users"""
+    pass
+
+
+# for command in users_cli.main.commands:
+for command in users_cli.main.commands:
+    users.add_command(users_cli.main.get_command(ctx=None, cmd_name=command))
+
+# load plug-ins from entry_points
+for ep in entry_points().get("gen3.plugins", []):
+    ep.load()
