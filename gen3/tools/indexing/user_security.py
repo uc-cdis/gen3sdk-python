2b6e58cb5bfddc67f6b5f986e1d3224ad00bc6f6
jacob50231 <jacob50231@gmail.com>
2024-05-23 10:12:31 -0500

    Added changes proposed in standup

--- gen3/tools/indexing/user_security.py
+++ gen3/tools/indexing/user_security.py
@@ -5,7 +5,8 @@ Module for checking user yaml configurations for security concerns
 import yaml
 import requests
 from os import environ
-from sys import exit
+from sys import exit, stdin
+from select import select
 from io import StringIO
 from cdislogging import get_logger
 
@@ -13,17 +14,20 @@ from cdislogging import get_logger
 logging = get_logger("__name__", log_level="info")
 
 
-def check_yaml(phsid, path, repo="commons-users", verbose=False):
+def check_yaml(phsids, path, repo="commons-users", verbose=False):
     """
     Scans a user.yaml file for occurrences of a given phsid to check if it is open access
     Must store a fine-grain github PAT with read access to the commons-user repository
     `export USER_YAML_GITHUB_PAT=<your-token>`
     Args:
-        phsid (str): the ID of the project being checked for open access
+        phsids (list): A list of phsids to check for
         path (str): the relative path from the repository the user.yaml file is hosted on
         repo (str): the repository the user.yaml file is hosted on
         verbose (bool): if true scans entire user.yaml file and finds any instance of phsid
     """
+
+    if type(phsids) == str:
+        phsids = [phsids]
     token = environ.get("USER_YAML_GITHUB_PAT")
     owner = "uc-cdis"
 
@@ -48,72 +52,105 @@ def check_yaml(phsid, path, repo="commons-users", verbose=False):
     except KeyError:
         auth_meth = "authz"
 
-    if verbose:
-        f = StringIO(response.content.decode())
-        logging.info(f"========= Scanning file for {phsid} occurence =========")
+    # Store whether any instance of any phsid has been found
+    anything_found = False
+
+    # Iterate through phsids
+    for phsid in phsids:
+        # Scans for any occurrence of phsid
+        if verbose:
+            lines = []
+            f = StringIO(response.content.decode())
+            logging.info(f"========= Scanning file for {phsid} occurrence =========")
+            found = False
+
+            # Find every line number containing the phsid
+            for no, line in enumerate(f):
+                if phsid in line:
+                    lines.append(str(no + 1))
+                    found = True
+                    anything_found = True
+
+            if found == False:
+                logging.info(f"{phsid} not found in file \n")
+                # If phsid isn't in the file we can continue to the next phsid
+                continue
+
+            # Log locations of phsid
+            else:
+                lines = ", ".join(lines)
+                logging.info(f"{phsid} found in lines {lines} \n")
+
+        logging.info(f"========= Scanning file for {phsid} in open projects =========")
         found = False
-        for no, line in enumerate(f):
-            if phsid in line:
-                logging.info(f"{phsid} found in line number {no + 1}")
-                found = True
-        if found == False:
-            logging.info(f"{phsid} not found in file")
+        # Track down open_data_reader
+        policies = content[auth_meth]["policies"]
+        for i in range(len(policies)):
+            if policies[i]["id"] == "open_data_reader":
+                policy = policies[i]
+                break
 
-    logging.info("\n")
+        # Check if phsid is hardcoded into the open data reader resource path
+        if (
+            phsid in policy["resource_paths"]
+            or "/programs/" + phsid in policy["resource_paths"]
+        ):
+            logging.info("PHSID Directly in Open Data Reader Resource Path")
 
-    logging.info(f"========= Scanning file for {phsid} in open projects =========")
-    found = False
-    # Track down open_data_reader
-    policies = content[auth_meth]["policies"]
-    for i in range(len(policies)):
-        if policies[i]["id"] == "open_data_reader":
-            policy = policies[i]
-            break
-
-    # Check if phsid is hardcoded into the open data reader resource path
-    if (
-        phsid in policy["resource_paths"]
-        or "/programs/" + phsid in policy["resource_paths"]
-    ):
-        logging.info("PHSID Directly in Open Data Reader Resource Path")
-
-    # Check if phsid is in one of the resource paths
-    else:
-        resources = content[auth_meth]["resources"]
-        open_resources = policy["resource_paths"]
-        for i in range(len(open_resources)):
-            resource = resources
-            path = open_resources[i].split("/")
-            for j in range(len(path)):
-                for k in range(len(resource)):
-                    if resource[k]["name"] == path[j]:
-                        try:
-                            resource = resource[k]["subresources"]
-                            break
-                        except KeyError:
-                            break
-
-            for j in range(len(resource)):
-                if resource[j]["name"] == "projects":
-                    resource = resource[j]["subresources"]
+        # Check if phsid is in one of the resource paths
+        else:
+            resources = content[auth_meth]["resources"]
+            open_resources = policy["resource_paths"]
+            for i in range(len(open_resources)):
+                resource = resources
+                path = open_resources[i].split("/")
+                for j in range(len(path)):
                     for k in range(len(resource)):
-                        if resource[k]["name"] == phsid:
-                            found = True
-                            location = "resources" + "/".join(path)
-                            logging.info(
-                                f"PHSID Found in {location} listed as an open program"
-                            )
+                        if resource[k]["name"] == path[j]:
+                            try:
+                                resource = resource[k]["subresources"]
+                                break
+                            except KeyError:
+                                break
 
-        if found == False:
-            logging.info(f"{phsid} not found in open projects")
+                locs = []
+                for j in range(len(resource)):
+                    if resource[j]["name"] == "projects":
+                        resource = resource[j]["subresources"]
+                        for k in range(len(resource)):
+                            if resource[k]["name"] == phsid:
+                                found = True
+                                anything_found = True
+                                location = "resources" + "/".join(path)
+                                locs.append(location)
 
-        continue_input = ""
-        while continue_input.lower() not in ["y", "n"]:
-            continue_input = input("Would you like to continue? (y/n) ")
-            if continue_input.lower() == "n":
+            if found == True:
+                logging.info(f"{phsid} found in {*locs,} listed as open\n")
+
+            if found == False:
+                logging.info(f"{phsid} not found in open projects \n")
+                if verbose == False:
+                    # If verbose is true and no phsid is found we would have already continued
+                    continue
+
+    # If we find anything ask for input (eventually times out)
+    if anything_found:
+        print("Would you like to continue? (y/n): ", end="", flush=True)
+        timeout = 5
+        i = None
+        i, o, e = select([stdin], [], [], timeout)
+
+        if i:
+            user_response = stdin.readline().strip()
+            logging.info(f"User Response: {user_response}")
+            if user_response != "y":
                 exit(1)
-        # If we reach the end of open resources without finding the phsid then return from the function
-        return
+        else:
+            logging.info("No user response")
+            exit(1)
+
+    # If we reach the end of the function we can return
+    return
 
 
 if __name__ == "__main__":
@@ -143,6 +180,7 @@ if __name__ == "__main__":
     args.add_argument(
         "--phsid",
         type=str,
+        nargs="+",
     )
 
     args = args.parse_args()
