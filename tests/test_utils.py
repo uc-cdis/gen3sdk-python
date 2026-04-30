f95dc069c64cf73cff84ceb2483a2e71c7f428da
WilsonnnTan <wilsonangelietann@gmail.com>
2026-05-01 00:03:07 +0700

    test: add test for unexpected order of `v`, `p`, and `c`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -56,7 +56,46 @@ from gen3.external.nih.utils import get_dbgap_accession_as_parts
             'consent_number': '3'
         }
     ),
-
+    # Unexpected order of v, p, and c
+    (
+        'phs000123.p3.v1.c3',
+        {
+            'phsid': 'phs000123',
+            'phsid_number': '000123',
+            'version': '',
+            'version_number': '',
+            'participant_set': 'p3',
+            'participant_set_number': '3',
+            'consent': '',
+            'consent_number': ''
+        }
+    ),
+    (
+        'phs000123.v3.c1.p3',
+        {
+            'phsid': 'phs000123',
+            'phsid_number': '000123',
+            'version': 'v3',
+            'version_number': '3',
+            'participant_set': '',
+            'participant_set_number': '',
+            'consent': 'c1',
+            'consent_number': '1'
+        }
+    ),
+    (
+        'phs000123.p3.v1',
+        {
+            'phsid': 'phs000123',
+            'phsid_number': '000123',
+            'version': '',
+            'version_number': '',
+            'participant_set': 'p3',
+            'participant_set_number': '3',
+            'consent': '',
+            'consent_number': ''
+        }
+    ),
 ])
 def test_get_dbgap_accession_as_parts(test_input, expected):
     """
